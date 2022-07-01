505fbe8fda0e828b9d1715b7ee4ff7c1ec3514f2
Andree Panjaitan <panjaitanandree@gmail.com>
2022-07-01 09:34:43 +0700

    Change access

--- middleware/fileMiddleware.js
+++ middleware/fileMiddleware.js
@@ -4,19 +4,21 @@ import { errorLog } from "../utils/logger.js";
 
 export default async (req, res, next) => {
   try {
-    const key = req.query.key;
-    const id = req.query.id;
-    if (id !== undefined && key !== undefined) {
-      let user = await User.findById(mongoose.Types.ObjectId(id));
-      let email = req.url.split("/")[1];
-      if (key === user.key && email === user.email) {
-        next();
-      } else {
-        throw Error("Key not same");
-      }
-    } else {
-      throw Error("Key or Id not found");
-    }
+    // const key = req.query.key;
+    // const id = req.query.id;
+    // if (id !== undefined && key !== undefined) {
+    //   let user = await User.findById(mongoose.Types.ObjectId(id));
+    //   let email = req.url.split("/")[1];
+    //   if (key === user.key && email === user.email) {
+    //     next();
+    //   } else {
+    //     throw Error("Key not same");
+    //   }
+    // } else {
+    //   throw Error("Key or Id not found");
+    // }
+
+    next();
   } catch (error) {
     errorLog(error);
     return res.status(401).json({
