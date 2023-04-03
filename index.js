f737bee2dac095297eb0d3219a5128fb7a99cb04
Andree Panjaitan <andreepanjaitan@Andrees-MacBook-Pro.local>
2023-04-04 00:32:50 +0700

    update master

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 const ipaddress = ip.address();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(morgan("dev"));
